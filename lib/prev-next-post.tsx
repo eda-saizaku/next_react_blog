68d2c3429414579a2a0e2bb79f71768ff0ba09aa
yumelog <felicita.amore.u@gmail.com>
2023-01-03 23:51:51 +0900

    add pagination

---
+++ lib/prev-next-post.tsx
@@ -0,0 +1,18 @@
+type Slug = {
+  title: string
+  slug: string
+}
+type Slugs = Slug[]
+
+export default function prevNextPost(allSlugs: Slugs, currentSlug: string) {
+  const numberOfPosts = allSlugs.length
+
+  const index = allSlugs.findIndex(({ slug }) => slug === currentSlug)
+
+  const prevPost =
+    index + 1 === numberOfPosts ? { title: '', slug: '' } : allSlugs[index + 1]
+
+  const nextPost = index === 0 ? { title: '', slug: '' } : allSlugs[index - 1]
+
+  return [prevPost, nextPost]
+}
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,27 +1,116 @@
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import Image from 'next/image'
+import ConvertBody from 'components/convert-body'
+import PostBody from 'components/post-body'
+import PostCategories from 'components/post-categories'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from 'components/two-column'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
+import { eyecatchLocal } from 'lib/constants'
+import { getPlaiceholder } from 'plaiceholder'
+import prevNextPost from 'lib/prev-next-post'
+import Pagination from 'components/pagination'
 
-type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+type Props = InferGetServerSidePropsType<typeof getStaticProps>
 
-const Page = ({ slug, page }: Props) => {
+export default function Post({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description,
+  prevPost,
+  nextPost,
+}: Props) {
   return (
-    <div>
-      <p>Slug is {slug}.</p>
-      {page && <p>Page is {page}.</p>}
-    </div>
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=""
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
+      </article>
+    </Container>
   )
 }
 
-export const getServerSideProps = async ({
-  params,
-  query,
-}: GetServerSidePropsContext<{ slug: string }>) => {
-
-  const slug = params?.slug || null
-  const page = query?.page || null
-
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs()
+  type Slug = {
+    slug: string
+  }
   return {
-    props: { slug, page },
+    paths: allSlugs.map(({ slug }: Slug) => `/blog/${slug}`),
+    fallback: false,
   }
 }
+export async function getStaticProps(context: GetServerSidePropsContext) {
+  const slug = context.params?.slug as string
+  const post = (await getPostBySlug(slug)) as any //TODO: as anyってどうなんだろう？
+  const content = post.content
+  const description = extractText(content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
-export default Page
+  const allSlugs = await getAllSlugs()
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: eyecatch,
+      categories: post.categories,
+      description: description,
+      prevPost: prevPost,
+      nextPost: nextPost,
+    },
+  }
+}
